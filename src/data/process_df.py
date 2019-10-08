37fa91be80a46b6239845a9552793d61cfd86c46
Quirijn Langedijk <gitkraken@quirijnlangedijk.nl>
2019-10-08 15:09:51 +0200

    Revert "Added Logistic Regression"
    This reverts commit d365906560127dd6eb2fa3679a495abecf2b25ba.

--- src/data/process_df.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-def process_df(df):
-    df.drop(['Additional_Number_of_Scoring', 'Review_Date', 'Reviewer_Nationality', 'Total_Number_of_Reviews', 'Total_Number_of_Reviews_Reviewer_Has_Given', 'days_since_review', 'lat', 'lng'], inplace=True, axis=1)
-    return df
